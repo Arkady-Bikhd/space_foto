3a2265a231c6e409f6e989595d590a00a82afdbd
Arkadiy <capark74@mail.ru>
2022-12-31 17:53:36 +0300

    Удалена лишняя функция

--- fetch_spacex_images.py
+++ fetch_spacex_images.py
@@ -4,9 +4,9 @@ import argparse
 
 def main():
     
-    spacex_id = create_parser_link()
+    spacex_id = create_parser_number()
     try:    
-        fetch_spacex_last_launch(spacex_id)
+        fetch_spacex_launch(spacex_id)
     except requests.exceptions.HTTPError:
         print('Неверная ссылка на запуск SpaceX')
     
@@ -26,34 +26,33 @@ def get_spacex_launch_links_latest():
     url = 'https://api.spacexdata.com/v5/launches/latest'
     response = requests.get(url)
     response.raise_for_status()
-    return response.json()['links']['flickr']['original']
-        
+    return response.json()['links']['flickr']['original']        
 
 
-def fetch_spacex_last_launch(spacex_id):
+def fetch_spacex_launch(spacex_id):
     
-    default_id = 66
-    images_links = get_spacex_launch_links(spacex_id)
+    images_links = None
+    if spacex_id:
+        images_links = get_spacex_launch_links(spacex_id)
     if not images_links:
         print(f'Фотографий запуска с номером {spacex_id} не найдено.')
         print('Осуществляется поиск фотографий последнего запуска.')
         images_links = get_spacex_launch_links_latest()        
-        if not images_links:
-            print('Фотографии последнего запуска не найдены.')
-            print('Загружаются фотографии запуска по умолчанию.')
-            images_links = get_spacex_launch_links(default_id)
-    image_dir = 'images'
-    for link_number, link in enumerate(images_links):
-        image_file = f'spacex{link_number}.jpg'        
-        get_image(link, image_file, image_dir)
+    if images_links:    
+        image_dir = 'images'
+        for link_number, link in enumerate(images_links):
+            image_file = f'spacex{link_number}.jpg'        
+            get_image(link, image_file, image_dir)
+    else:
+        print('Фотографии не найдены')
 
 
-def create_parser_link():
+def create_parser_number():
 
     parser = argparse.ArgumentParser(
         description='Программа скачивает фотографии запусков SpaceX'
     )
-    parser.add_argument('-n', '--number', default='66', help='Введите номер запуска')
+    parser.add_argument('-n', '--number', required=False, help='Введите номер запуска')
     args = parser.parse_args()
 
     return args.number
